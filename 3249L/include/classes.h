1886768e8b0bc6ee694b9e723d12e391727ac8ae
memes4daysz9 <73305439+memes4daysz9@users.noreply.github.com>
2024-05-03 13:18:40 -0700

    edge died

--- 3249L/include/classes.h
+++ 3249L/include/classes.h
@@ -4,30 +4,39 @@ class Bot{
     public:
     int RDeg;
     int LDeg;
-    int LTar;
-    int RTar;
+    double LTar;
+    double RTar;
+    double Dis;
+    double LDis;
+    double RDis;
     double error;
+    bool PurePursuit;
+    double Heading;
     Bot();
     void RelativeForward(float Distance);
     void RelativeRotate(float Deg);
     void RelativeHardStop(float Distance);
+    void RelativeCurve(int[1] p1,int[1] p2);
     void AbsoluteForward(int x,int y);
     void AbsoluteRotate(float Deg);
     void AbsoluteHardStop(int x, int y);
+    void AbsoluteCurve(int[1] p1,int[1] p2, int[1] p3);
+
 };
 
 class Lambda{
     public:
     bool status;
     Lambda();
-    void Activate();
-    void Deactivate();
+    void Activate();//Lambda.Activate();
+    void Deactivate();//Lambda.DeActivate();
 };
 
 class Conveyor{
     public:
     bool Climbing;
+    bool Intaking;
     Conveyor();
-    void Climb();
-    void Intake();
+    void Climb();//Conveyor.Climb();
+    void Intake();//Conveyor.Intake();
 };
--- 3249L/src/Tracking.cpp
+++ 3249L/src/Tracking.cpp
@@ -1,1 +1,49 @@
 #include "main.h"
+
+const double radius;
+
+class Bot(){
+    int RDeg;
+    int LDeg;
+    double LTar;
+    double RTar;
+    double Dis;
+    double LDis;
+    double RDis;
+    double error;
+    double Heading;
+    bool PurePursuit;
+}
+double DegToRad(double x) {
+    return (x * (pi/180));
+}
+double RadToDeg(double x) {
+    return (x* (180/pi));
+}
+
+void Odometry(){
+    double DeltaLeft;
+    double DeltaRight;
+    double LastL;
+    double LastR;
+
+    while (1){
+        Bot.RDeg = 1;//replace with Motor degrees
+        Bot.LDeg = 1;
+
+        DeltaLeft = (Bot.LDeg - LastL);
+        DeltaRight = (Bot.RDeg - LastR);
+
+        Bot.LDis = DegToRad(DeltaLeft) * radius;
+        Bot.RDis = DegToRad(DeltaRight) * radius;
+
+        Bot.Dis = (Bot.LDis + Bot.RDis)/2.0;
+        Bot.Heading = (Bot.Heading - LastHeading);
+
+        Bot.x = Bot.Dis * cos(Bot.Heading);
+        Bot.y = Bot.Dis * sin(Bot.Heading);
+
+        LastL = Bot.LDeg;
+        LastR = Bot.RDeg;
+    }
+}
