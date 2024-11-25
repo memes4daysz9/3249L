0845f1350ce0c0b40ded98cdc5d3a53f685a84a7
memes4daysz9 <benj08082008@icloud.com>
2024-11-24 20:54:44 -0800

    added binary uploding and motor temp checks

--- 3249L/src/NotNeededFeatures.cpp
+++ 3249L/src/NotNeededFeatures.cpp
@@ -22,3 +22,56 @@ void ToggleBrakeMode(){
     BLXMotor.set_brake_mode(BrakeMode[Stepper]);
     BRXMotor.set_brake_mode(BrakeMode[Stepper]);
 }
+
+void MoterTempCheck(){
+    const uint8_t max = 60;
+
+    pros::Motor FLXMotor(-1);
+    pros::Motor FRXMotor(2);
+    pros::Motor BLXMotor(3); // testing
+    pros::Motor BRXMotor(-7);
+
+    float FrontMax = std::max(FLXMotor.get_temperature(),FRXMotor.get_temperature());
+    float BackMax = std::max(BLXMotor.get_temperature(), BRXMotor.get_temperature());
+
+    float AllMax = std::max(FrontMax, BackMax);
+    if(AllMax > max){
+        bot.ErrorSector = ((FLXMotor.get_temperature() >= max) *1000)+ ((FRXMotor.get_temperature() >= max) * 100) + ((BLXMotor.get_temperature() >= max)*10) + (BRXMotor.get_temperature() >= max);
+        //longest statement ever
+    }
+    /*if(AllMax >= 60){
+        if(FrontMax > BackMax){
+
+            if (FLXMotor.get_temperature() > FRXMotor.get_temperature())
+            {
+             
+                bot.ErrorSector = 1;
+           
+            }else{
+
+                bot.ErrorSector = 2;
+
+            }
+            
+
+        }else{
+
+             if (BLXMotor.get_temperature() > BRXMotor.get_temperature())
+            {
+             
+                bot.ErrorSector = 3;
+           
+            }else{
+
+                bot.ErrorSector = 4;
+
+            }
+
+        }
+
+       
+       bot.Interupt = true;
+
+    }*/
+    
+}
